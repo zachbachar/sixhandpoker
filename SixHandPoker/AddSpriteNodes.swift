fc1f7a0d14b898729087a8548cbdf95ffbf8d513
Zach Bachar <zachbachar@Zachs-MacBook-Pro.local>
2017-11-29 15:36:37 +0200

    double sided card + a lot of code reArrangment

---
+++ SixHandPoker/AddSpriteNodes.swift
@@ -0,0 +1,9 @@
+//
+//  AddSpriteNodes.swift
+//  SixHandPoker
+//
+//  Created by Zach Bachar on 29/11/2017.
+//  Copyright © 2017 zach bachar. All rights reserved.
+//
+
+import Foundation
---
+++ SixHandPoker/FirstDraw.swift
@@ -0,0 +1,9 @@
+//
+//  FirstDraw.swift
+//  SixHandPoker
+//
+//  Created by Zach Bachar on 29/11/2017.
+//  Copyright © 2017 zach bachar. All rights reserved.
+//
+
+import Foundation
